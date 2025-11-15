ad8497a8055f869a8c4efff3303c5d948c873252
Rick's Mid-Life Crisis <ricksmidlifecrisis@gmail.com>
2025-11-15 11:46:20 +1000

    Commented out debug messages on server.

--- Workshop/Contents/mods/RicksMLC_Flies/media/lua/server/RicksMLC_FliesServer.lua
+++ Workshop/Contents/mods/RicksMLC_Flies/media/lua/server/RicksMLC_FliesServer.lua
@@ -10,18 +10,18 @@ RicksMLC_FliesServer.CurrentFliesStatus = {}
 function RicksMLC_FliesServer.HandleClientSmellUpdate(module, command, player, args)
     if module == "RicksMLC_FliesServer" then
         if command == "UpdateSmell" then
-            DebugLog.log(DebugType.Mod, "RicksMLC_FliesServer.HandleClientSmellUpdate() Update Smell")
+            --DebugLog.log(DebugType.Mod, "RicksMLC_FliesServer.HandleClientSmellUpdate() Update Smell")
             RicksMLC_FliesServer.CurrentFliesStatus[args.OnlineId] = args.FliesOn    
-            RicksMLC_SharedUtils.DumpArgs(RicksMLC_FliesServer.CurrentFliesStatus, 0, "Server Current Flies Status")
+            --RicksMLC_SharedUtils.DumpArgs(RicksMLC_FliesServer.CurrentFliesStatus, 0, "Server Current Flies Status")
         elseif command == "CurrentPlayersFlies" then
-            DebugLog.log(DebugType.Mod, "RicksMLC_FliesServer.HandleClientSmellUpdate() request CurrentPlayersFlies")
+            --DebugLog.log(DebugType.Mod, "RicksMLC_FliesServer.HandleClientSmellUpdate() request CurrentPlayersFlies")
         end
         if RicksMLC_FliesServer.CurrentFliesStatus ~= {} then
             local retArgs = {PlayerFliesList = RicksMLC_FliesServer.CurrentFliesStatus}
             sendServerCommand("RicksMLC_Flies", "UpdateSmellFromServer", retArgs)
             RicksMLC_SharedUtils.DumpArgs(retArgs, 0, "RicksMLC_Flies: UpdateSmellFromServer retArgs")
         else
-            DebugLog.log(DebugType.Mod, "RicksMLC_FliesServer.HandleClientSmellUpdate: No current flies status - not sending reply")
+            --DebugLog.log(DebugType.Mod, "RicksMLC_FliesServer.HandleClientSmellUpdate: No current flies status - not sending reply")
         end
     end
 end
